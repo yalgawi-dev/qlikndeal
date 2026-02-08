13e2452d219500e858747d514a57a24a09727b7e
Yehuda Algawi <yalgawi@gmail.com>
2026-02-08 02:18:24 +0200

    Fix missing useEffect import

--- src/components/ReferralCard.tsx
+++ src/components/ReferralCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Copy, Facebook, Share2, Twitter, Check } from "lucide-react";
 
